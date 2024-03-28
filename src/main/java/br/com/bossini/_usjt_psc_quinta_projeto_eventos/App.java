93585e1397569f1e50a87837e904b7f0b06c43ef
Rodrigo Bossini <professorbossini@gmail.com>
2024-03-28 20:07:04 -0300

    feat(sim-proj): implementa a tela de login

---
+++ src/main/java/br/com/bossini/_usjt_psc_quinta_projeto_eventos/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.bossini._usjt_psc_quinta_projeto_eventos;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
