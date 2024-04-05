b0a11ec61141a65a1cc55b81dde6e4cc912a6ecc
Rodrigo Bossini <professorbossini@gmail.com>
2024-04-04 21:43:09 -0300

    feat(sim-proj): cria o banco de dados
    - cria a tabela de usuário

--- script.sql
+++ script.sql
@@ -1,26 +1,24 @@
---criar uma tabela para usuário
-CREATE TABLE tb_usuario(
- cod_usuario SERIAL PRIMARY KEY,
- login VARCHAR(200) NOT NULL,
- senha VARCHAR(200) NOT NULL
+-- Active: 1712273957933@@pg-33ad0904-professorbossini.a.aivencloud.com@12956@defaultdb@public
+
+CREATE TABLE tb_usuario_psc_quinta(
+  cod_usuario SERIAL PRIMARY KEY,
+  login VARCHAR(200) NOT NULL,
+  senha VARCHAR(200) NOT NULL
 );
-
 --cadastrar um usuário
-INSERT INTO tb_usuario
+INSERT INTO tb_usuario_psc_quinta
 (login, senha) VALUES
-('admin', 'admin');
+('comum', 'comum');
 
--- listar usuários existentes
-SELECT * FROM tb_usuario;
--- apenas duas colunas(projeção)
-SELECT login, senha FROM tb_usuario;
---pegando dados apenas da Ana
-SELECT * FROM tb_usuario WHERE cod_usuario = 2;
+SELECT  *
+FROM tb_usuario_psc_quinta;
 
-SELECT * FROM tb_usuario ORDER BY login;
---atualizar dados de um usuário
-UPDATE tb_usuario SET login = 'Ana Paula'
-	WHERE cod_usuario = 2;
---apagar um usuário
-DELETE FROM tb_usuario WHERE cod_usuario = 1;
+UPDATE tb_usuario_psc_quinta SET
+login='Admin',senha='Admin'
+WHERE cod_usuario = 1;
 
+DELETE FROM tb_usuario_psc_quinta
+WHERE cod_usuario = 2;
+
+UPDATE tb_usuario_psc_quinta SET
+senha='' WHERE cod_usuario = 1;
